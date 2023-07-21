96be1ec0045a3a7d45061fa11ca26221ed41e42b
mimhle <40392166+mimhle@users.noreply.github.com>
2023-07-21 15:57:41 +0700

    Added progress bar

--- unzip_in_place.py
+++ unzip_in_place.py
@@ -10,15 +10,16 @@ Limitations:
 import os
 import argparse
 import zipfile
+import tqdm
 
 
 def unzip_files(
         zip_file_path: str,
         destination_dir: str,
+        save_to_subfolder: bool = False,
         encoding: str = "utf-8",
         decoding: str = "utf-8",
         include_ext: list = None,
-        save_to_subfolder: bool = False
 ) -> list:
     zipdata = zipfile.ZipFile(zip_file_path)
     zipinfos = zipdata.infolist()
@@ -33,7 +34,7 @@ def unzip_files(
         os.makedirs(destination_dir, exist_ok=True)
 
     for zipinfo in zipinfos:
-        zipinfo.filename = '_'.join(zipinfo.filename.encode(encoding).decode(decoding).split())
+        zipinfo.filename = ' '.join(zipinfo.filename.encode(encoding).decode(decoding).split())
         extension = os.path.splitext(zipinfo.filename)[-1].lower()
         if include_ext is None or extension in include_ext:
             zipdata.extract(zipinfo, path=destination_dir)
@@ -46,20 +47,29 @@ def unzip_files(
 
 
 def extract_all_zips(path: str, save_to_subfolder: bool = True) -> None:
+
+    file_to_extract = []
+
+    # find all zip files
     for root, dirs, files in os.walk(path):
         for file in files:
             if file.endswith(".zip"):
                 zip_file_path = os.path.join(root, file)
-                print("Extracting:", zip_file_path)
-                unzip_files(zip_file_path, root, save_to_subfolder=save_to_subfolder)
-                os.remove(zip_file_path)
+                file_to_extract.append((zip_file_path, root, save_to_subfolder))
+
+    # unzip and delete  all files
+    for zip_file_path, root, save_to_subfolder in tqdm.tqdm(file_to_extract):
+        unzip_files(zip_file_path, root, save_to_subfolder=save_to_subfolder)
+        os.remove(zip_file_path)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Extract all zip files in a folder and delete the zips after unzip")
     parser.add_argument("path_to_folder", type=str, help="path to folder")
+    parser.add_argument("-s", "--save_to_subfolder", action="store_true", help="save extracted files to subfolder")
     args = parser.parse_args()
 
     extract_all_zips(
         args.path_to_folder,
+        save_to_subfolder=args.save_to_subfolder
     )
